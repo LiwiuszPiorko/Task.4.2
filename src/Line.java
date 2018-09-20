06653f5165dc67580c539e62b6c2c83f4114cca8
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-09-20 14:29:03 +0200

    Pierwsza wersja

---
+++ src/Line.java
@@ -0,0 +1,28 @@
+public class Line {
+
+    public Point start;
+    public Point end;
+
+
+
+    public Line(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public Point getStart() {
+        return start;
+    }
+
+    public void setStart(Point start) {
+        this.start = start;
+    }
+
+    public Point getEnd() {
+        return end;
+    }
+
+    public void setEnd(Point end) {
+        this.end = end;
+    }
+}
---
+++ src/Method.java
@@ -0,0 +1,24 @@
+public class Method {
+
+
+
+    public boolean isLonger(Line line) {
+
+        int line1=(line.start*line.start)
+
+        if(){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
